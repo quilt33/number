e3be4974730cba58b099d37238080aa140bb5fb3
quilt33 <yblyyll@gmail.com>
2025-07-09 04:03:15 +0200

    submit.php

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "mydata"; // اسم قاعدة البيانات
+$dbname = "user_paidads"; // تم تعديل اسم قاعدة البيانات
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
